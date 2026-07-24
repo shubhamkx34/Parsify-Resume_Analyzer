ae4f597923f1f64ce723596f2c7822e048c389b0
Shubham <shubhamkx34@gmail.com>
2026-07-24 11:50:50 +0530

    setup state management react layer (contextApi)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 //Imports <RouterProvider>, a special React component whose only job is to handle and display routes.
-import { RouterProvider } from "react-router"
-import { router } from "./app.routes.jsx"
+import { RouterProvider } from "react-router";
+import { router } from "./app.routes.jsx";
+import { AuthProvider } from "./auth/auth.context.jsx";
 
 function App() {
   return (
-//renders whichever page matches the browser's current URL using <RouterProvider>.  
-    <RouterProvider router = {router}/> 
-  )
+    //renders whichever page matches the browser's current URL using <RouterProvider>.
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
-export default App
-
+export default App;
--- Frontend/src/auth/auth.context.jsx
+++ Frontend/src/auth/auth.context.jsx
@@ -1,2 +1,13 @@
 //THis File lies under state layer which helps to store the logged-in user data and manage loading while an api is called.
+//It allows you to share user data across your entire app without passing variables manually from parent to child components.
 
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setuser] = useState(null);
+  const [loading, setloading] = useState(false);
+
+  return <AuthContext.provider value={{ user, setuser, loading, setloading }}>{children}</AuthContext.provider>;
+};
